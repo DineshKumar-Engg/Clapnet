b380dbd5899703c61495564adbebbc7cef451858
DineshKumar-Engg <dineshsoftwareengg@gmail.com>
2025-05-17 02:29:28 +0530

    port changed

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Clapnet Backend!');
 });
 
-app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+      console.log(`🚀 Server running on  http://0.0.0.0:${PORT}`);
 });
